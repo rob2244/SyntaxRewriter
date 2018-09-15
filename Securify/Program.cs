5fbd032c911ecb61579d045a8c5bef623a9989de
Robin Seitz <robinseitz@hotmail.com>
2018-09-15 16:56:37 -0700

    Add project files.

---
+++ Securify/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Securify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
