6fc6fd024aa4550fc3faa82cea2c1491d37de89a
Robin Seitz <robinseitz@hotmail.com>
2018-09-16 10:49:50 -0700

    initial commit

--- Securify/Program.cs
+++ Securify/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Securify.Console;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace Securify
 {
@@ -6,7 +11,42 @@ namespace Securify
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dir = GetDirectory(args);
+
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"{dir.FullName} could not be found");
+
+            var controllerDir = dir.EnumerateDirectories("controllers", SearchOption.AllDirectories).FirstOrDefault();
+
+            if (controllerDir == null || !controllerDir.Exists)
+                throw new DirectoryNotFoundException($"Controller directory not found");
+
+            var rewriter = new TokenAttributeSyntaxRewriter();
+
+            foreach (var file in controllerDir.EnumerateFiles())
+                Rewrite(file, rewriter);
+
+            System.Console.WriteLine("Token rewrite completed successfully");
+            System.Console.ReadKey();
+        }
+
+
+
+        private static DirectoryInfo GetDirectory(string[] args)
+        {
+            if (args.Length == 0)
+                return new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            return new DirectoryInfo(args[0].ToString());
+        }
+
+        private static void Rewrite(FileInfo file, TokenAttributeSyntaxRewriter rewriter)
+        {
+            var sourceTree = CSharpSyntaxTree.ParseText(File.ReadAllText(file.FullName));
+            var newTree = rewriter.Visit(sourceTree.GetRoot());
+
+            if (newTree != sourceTree.GetRoot())
+                File.WriteAllText(file.FullName, newTree.ToString());
         }
     }
 }
---
+++ Securify/TokenAttributeSyntaxRewriter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Securify.Console
+{
+    public class TokenAttributeSyntaxRewriter : CSharpSyntaxRewriter
+    {
+        const string attributeName = "Token Authorize";
+
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            var attribute = node.AttributeLists.SelectMany(c => c.Attributes)
+                .FirstOrDefault(a => (a.Name as IdentifierNameSyntax)
+                    .Identifier.Text.StartsWith(attributeName));
+
+
+            if (node.AttributeLists.Any(al => al.Contains(attribute)))
+                return node;
+
+            var newAttrib =
+                AttributeList(
+                            SingletonSeparatedList(
+                                Attribute(
+                                    IdentifierName(attributeName)
+                                    )
+                                )
+                            );
+
+            return node.AddAttributeLists(newAttrib).NormalizeWhitespace();
+        }
+    }
+}
